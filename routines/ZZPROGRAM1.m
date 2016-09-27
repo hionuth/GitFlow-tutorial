b5ba114a409e4162b2900f367f14ea382e282c4b
Ionut Hrinca <ionut.hrinca@gmail.com>
2016-09-27 21:40:23 +0300

    Am modificat Programul 1 dar cineva mi-a luat-o inainte cu commitul

--- routines/ZZPROGRAM1.m
+++ routines/ZZPROGRAM1.m
@@ -1,5 +1,6 @@
 ZZPROGRAM1
 	;
 	w "Acesta este prima linie din program"
-	W "vom folosi acest program sa vedem cum lucram cu git"
+	W "vom folosi acest program sa vedem cum lucram cu git. In caz ca nu va merge merge-ul vom munci din greu"
+	W "linie noua"
 	Q
