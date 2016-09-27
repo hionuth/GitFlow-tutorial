dc9ade456bf76392693755d0bc7737d2d960f2ed
Ionut Hrinca <ionut.hrinca@gmail.com>
2016-09-27 21:34:10 +0300

    Am modificat Ptogramul 1 si am creat Programul 2

--- routines/ZZPROGRAM1.m
+++ routines/ZZPROGRAM1.m
@@ -2,4 +2,5 @@ ZZPROGRAM1
 	;
 	w "Acesta este prima linie din program"
 	W "vom folosi acest program sa vedem cum lucram cu git"
+	W "Am adaugat prima linie in branch-ul INGR-2 dar l-am modificat in INGR-3"
 	Q
